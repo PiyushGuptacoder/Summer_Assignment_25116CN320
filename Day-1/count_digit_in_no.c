3758866b77e2459cd72d60a8fcaec90f71975da4
piyushthecoder <piyushwinn@gmail.com>
2026-06-01 13:44:57 +0530

    day1 uploaded

---
+++ Day-1/count_digit_in_no.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main(){
+    int n;
+    int count=0;
+    printf("Enter the number to count digit: ");
+    scanf("%d",&n);
+    while(n>0){
+        n=n/10;
+        count++;
+    }
+    printf("The number of digit in the given number is %d",count);
+    return 0;
+
+}
