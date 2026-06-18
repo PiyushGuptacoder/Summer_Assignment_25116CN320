ac6d65327af464c167ec82980210a3dba202812b
piyushthecoder <piyushwinn@gmail.com>
2026-06-18 18:35:10 +0530

    day 18 uploaded

---
+++ Day-18/q72.c
@@ -0,0 +1,8 @@
+int main(){
+    int n,arr[100],target;
+    printf("Enter the size of array: ");
+    scanf("%d",&n);
+    for (int i=0;i<n;i++){
+        printf("Enter the element %d: ",i+1);
+        scanf("%d",&arr[i]);
+    }
